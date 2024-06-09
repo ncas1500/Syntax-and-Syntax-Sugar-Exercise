f916c3a2daecf77c62b37a9662219ff0fdc18d82
narciso castro <narcisocastro0802@gmail.com>
2024-06-09 00:57:34 -0700

    made adjustments to code

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -3,8 +3,55 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {/*
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " is greater than or equal to nine";
+            }
+
+            //Ternary Operator
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+
+            //String Interpolation
+
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = ($"{answer} is less than nine");
+            }
+            else
+            {
+                response = ($"{answer} is greater than or equal to nine");
+            }
+            Console.WriteLine(response)*/;
+
+            //Inferred Typing
+
+            var answer = 4;
+            var response = "";
+
+            if (answer < 9)
+            {
+                response = (answer + " is less than nine");
+            }
+            else
+            {
+                response = (answer + " is geater than or equal to nine");
+            }
+            Console.WriteLine(response);
         }
     }
 }
